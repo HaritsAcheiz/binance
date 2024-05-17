669cbabb273d31e6d31aa4c1345b42836a30f2e1
Muhammad Harits R <harits.muhammad.only@gmail.com>
2024-05-17 10:47:03 +0700

    not found, found using password, found using passkey, captcha, error are all handled

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
 from selenium.webdriver import Keys
 from seleniumbase import SB
 import pandas as pd
+from seleniumbase.common.exceptions import TextNotVisibleException
+
 
 def check_account(data, i):
     url = 'https://accounts.binance.com/en/login'
-    with SB(uc=True, incognito=True, proxy='udksqrvp-rotate:0irkbd10fjkn@p.webshare.io:80') as sb:
-        try:
-            sb.maximize_window()
+    while 1:
+        with SB(
+                uc=True,
+                incognito=True,
+                proxy='udksqrvp-rotate:0irkbd10fjkn@p.webshare.io:80',
+                maximize=True) as sb:
             sb.driver.uc_open_with_reconnect(url, reconnect_time=3)
             sb.type('input[name="username"]', text=data.iloc[i, 0])
             sb.driver.uc_click('button:contains(Next)')
-            # sb.click('input#email', timeout=3)
-            # sb.update_text('input#email', text)
-            # sb.click('button:contains(Next)', timeout=3)
-            if sb.assert_text('password'):
-                print(f'{data.iloc[i, 0]} is exist!!!')
-                data.iloc[i, -1] = True
-            elif sb.assert_text('Passkey'):
-                print(f'{data.iloc[i, 0]} is exist!!!')
-                data.iloc[i, -1] = True
-            elif sb.assert_text('passkey'):
-                print(f'{data.iloc[i, 0]} is exist!!!')
-                data.iloc[i, -1] = True
-            else:
+            try:
+                sb.assert_text('found', timeout=3)
                 print(f'{data.iloc[i, 0]} is not exist!!!')
                 data.iloc[i, -1] = False
-        except Exception as e:
-            print(f'{data.iloc[i, 0]} is not exist!!!')
-            data.iloc[i, -1] = False
-            # input('press any key')
+                break
+            except TextNotVisibleException:
+                try:
+                    sb.assert_text('password', timeout=3)
+                    print(f'{data.iloc[i, 0]} is exist!!!')
+                    data.iloc[i, -1] = True
+                    break
+                except TextNotVisibleException:
+                    try:
+                        sb.assert_text('passkey', timeout=3)
+                        print(f'{data.iloc[i, 0]} is exist!!!')
+                        data.iloc[i, -1] = True
+                        break
+                    except TextNotVisibleException:
+                        print('error found...retry')
 
 if __name__ == '__main__':
     data = pd.read_excel('binance test.xlsx', header=0, sheet_name='Sheet1')
     data['is_exist'] = None
     for i in range(len(data)):
-    # for i in range(2):
         check_account(data, i)
     data.to_csv('checked_email.csv')
-    #
-    # text = 'Hello@gmail.com'
