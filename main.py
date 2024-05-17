0b0fcd16b4a9f06869df31e7d30d328274bc8def
Muhammad Harits R <harits.muhammad.only@gmail.com>
2024-05-18 02:37:22 +0700

    ready to deploy

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os.path
+
 from selenium.webdriver import Keys
 from seleniumbase import SB
 import pandas as pd
 from seleniumbase.common.exceptions import TextNotVisibleException
+import numpy as np
 
 
 def check_account(data, i):
@@ -12,32 +15,49 @@ def check_account(data, i):
                 incognito=True,
                 proxy='udksqrvp-rotate:0irkbd10fjkn@p.webshare.io:80',
                 maximize=True) as sb:
-            sb.driver.uc_open_with_reconnect(url, reconnect_time=3)
+            sb.driver.uc_open_with_reconnect(url, reconnect_time=8)
             sb.type('input[name="username"]', text=data.iloc[i, 0])
-            sb.driver.uc_click('button:contains(Next)')
+            sb.driver.uc_click('button:contains(Next)', reconnect_time=8)
             try:
                 sb.assert_text('found', timeout=3)
                 print(f'{data.iloc[i, 0]} is not exist!!!')
-                data.iloc[i, -1] = False
+                data.iloc[i, -1] = 'No'
                 break
             except TextNotVisibleException:
                 try:
                     sb.assert_text('password', timeout=3)
                     print(f'{data.iloc[i, 0]} is exist!!!')
-                    data.iloc[i, -1] = True
+                    data.iloc[i, -1] = 'Yes'
                     break
                 except TextNotVisibleException:
                     try:
                         sb.assert_text('passkey', timeout=3)
                         print(f'{data.iloc[i, 0]} is exist!!!')
-                        data.iloc[i, -1] = True
+                        data.iloc[i, -1] = 'Yes'
                         break
                     except TextNotVisibleException:
-                        print('error found...retry')
+                        try:
+                            sb.assert_text('valid', timeout=3)
+                            print(f'{data.iloc[i, 0]} is not exist!!!')
+                            data.iloc[i, -1] = 'No'
+                            break
+                        # except TextNotVisibleException as e:
+                        except Exception as e:
+                            print('error found...retry')
+                            print(e)
+                            # input('press any key to continue')
 
 if __name__ == '__main__':
-    data = pd.read_excel('binance test.xlsx', header=0, sheet_name='Sheet1')
-    data['is_exist'] = None
+    if os.path.exists('checked_email.csv'):
+        data = pd.read_csv('checked_email.csv')
+    else:
+        data = pd.read_excel('binance test.xlsx', header=0, sheet_name='Sheet1')
+        data['is_exist'] = None
+    print(data)
     for i in range(len(data)):
-        check_account(data, i)
-    data.to_csv('checked_email.csv')
+        if pd.isna(data.iloc[i, -1]):
+            check_account(data, i)
+            print(data)
+            data.to_csv('checked_email.csv', index=False)
+        else:
+            continue
